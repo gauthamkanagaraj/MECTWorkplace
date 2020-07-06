00cfea2268f438e578847126358da319f890f963
gauthamkanagaraj <67670956+gauthamkanagaraj@users.noreply.github.com>
2020-07-06 21:44:56 +0530

    Add files via upload

--- js/signup2.js
+++ js/signup2.js
@@ -9,3 +9,20 @@ $(".add-image").on("click", function () {
 $(".next").on("click", function () {
 	window.location.replace("accountcreated.html");
 });
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    
+    reader.onload = function(e) {
+      $('.img').attr('src', e.target.result);
+    }
+    
+    reader.readAsDataURL(input.files[0]); // convert to base64 string
+	$(".add-image").hide();
+  }
+}
+
+$(".file").change(function() {
+  readURL(this);
+});
