a1dcc0468c007a037f41c6714488322d99236ff0
gauthamkanagaraj <67670956+gauthamkanagaraj@users.noreply.github.com>
2020-07-06 16:35:26 +0530

    Add files via upload

--- js/signup1.js
+++ js/signup1.js
@@ -2,3 +2,7 @@
  $(".dial").knob({
 	'width': 60
 	});		
+	
+$(".next").on("click", function () {
+	window.location.replace("signup2.html");
+});
---
+++ js/signup2.js
@@ -0,0 +1,11 @@
+$(".dial").knob({
+	'width': 60
+	});	
+	
+$(".add-image").on("click", function () {
+	$(".file").click();
+});
+
+$(".next").on("click", function () {
+	window.location.replace("accountcreated.html");
+});
